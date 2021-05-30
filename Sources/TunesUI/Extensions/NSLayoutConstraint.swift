92a626f68f4974d9a351693f8650404df413682a
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-05-30 19:33:52 +0200

    Make copyLayout more configurable.

--- Sources/TunesUI/Extensions/NSLayoutConstraint.swift
+++ Sources/TunesUI/Extensions/NSLayoutConstraint.swift
@@ -8,13 +8,24 @@
 import Cocoa
 
 public extension NSLayoutConstraint {
-	static func copyLayout(from container: NSView, for view: NSView) -> [NSLayoutConstraint] {
-		return [
-			NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1, constant: 0),
-			NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1, constant: 0),
-			NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1, constant: 0),
-			NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1, constant: 0),
+	static func copyLayout(
+		from container: NSView,
+		for view: NSView,
+		multiplier: [NSLayoutConstraint.Attribute: CGFloat] = [:],
+		constant: [NSLayoutConstraint.Attribute: CGFloat] = [:]
+	) -> [NSLayoutConstraint] {
+		let attributes: [NSLayoutConstraint.Attribute] = [
+			.leading, .trailing, .top, .bottom
 		]
+		
+		return attributes.map {
+			NSLayoutConstraint(
+				item: view, attribute: $0,
+				relatedBy: .equal,
+				toItem: container, attribute: $0,
+				multiplier: multiplier[$0] ?? 1, constant: constant[$0] ?? 0
+			)
+		}
 	}
 	
 	static func center(in container: NSView, for view: NSView) -> [NSLayoutConstraint] {
